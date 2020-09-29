88269ebbf4effe6a9a435fdb7b5056e20e854309
Gilberts ahumada <gilbertsahumada@gmail.com>
2020-09-29 19:23:43 -0300

    first commit

---
+++ database/migrations/2020_09_29_220706_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            //Varchar con largo 1000, si no es descrito el valor por defecto es 250
+            $table->string('description', 1000);
+            $table->float('price')->unsigned();
+            $table->integer('stock')->unsigned();
+            $table->string('status')->default('unavailable');            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
