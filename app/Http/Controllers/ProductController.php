dbe6db1e38ddeca10bcff3f21f0347a92d7a76f1
Gilberts ahumada <gilbertsahumada@gmail.com>
2020-09-30 08:55:15 -0300

    data selected by query builder in productController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,13 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     public function index()
     {
+        $products = DB::table('Products')->get();
+        dd($products);
         return view('products.index');
         //return 'This is the list of products from CONTROLLER';
     }
@@ -22,8 +24,13 @@ class ProductController extends Controller
     }
     public function show($product)
     {
+        //using first we'll get the information of the first row and not the collection
+        //$product = DB::table('Products')->where('id', $product)->first();
+        //other way to get the same result by the parameter $product
+        $product = DB::table('products')->find($product);
+        dd($product);
         return view('products.show');
-        return "Showing products with id {$product}";
+        //return "Showing products with id {$product}";
     }
     public function edit($product)
     {
