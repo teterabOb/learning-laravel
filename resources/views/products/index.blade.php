b9ffe204a56b94dedcb846c9bacdd9cd6cb860cc
Gilberts ahumada <gilbertsahumada@gmail.com>
2020-09-30 17:31:28 -0300

    adding layouts

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Products</title>
-</head>
-<body>
+
+@extends('layouts.master')
+@section('content')
     <h1>List of Products</h1>
 
     @empty($products)
@@ -43,5 +37,4 @@
         </table>
     </div>
     @endempty
-</body>
-</html>
+@endsection
--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,16 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Showing Detail</title>
-</head>
-<body>
+@extends('layouts.master')
+@section('content')
     <h1>{{ $product->title }} {{ $product->id }}</h1>
     <p>{{$product->description}}</p>
     <p>{{$product->price}}</p>
     <p>{{$product->stock}}</p>
     <p>{{$product->status}}</p>
-</body>
-</html>
+@endsection
+
+
+
