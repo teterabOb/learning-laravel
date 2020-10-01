39c5a9556afd93dd80df73e901e15c2b3e8a435e
Gilberts ahumada <gilbertsahumada@gmail.com>
2020-10-01 09:19:15 -0300

    new view and new features in productController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -17,11 +17,25 @@ class ProductController extends Controller
 
     public function create()
     {
-        return 'This is the form to create a product from CONTROLLER';
+        return view('products.create');
+        //return 'This is the form to create a product from CONTROLLER';
     }
     public function store()
     {
-        //
+        /* first way to create a new product 
+        $product = Product::create([
+            'title' => request()->title,
+            'description' => request()->description,
+            'price' => request()->price,
+            'stock' => request()->stock,
+            'status' => request()->status,
+        ]);
+        */
+
+        /*This way works because we've created an var fillable
+        in our Product model*/
+        $product = Product::create(request()->all());
+        return $product;
     }
     public function show($product)
     {
