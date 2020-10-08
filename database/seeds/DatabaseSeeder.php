ff28b70c013bb26001f2c5cf81a109e43844e622
Gilberts ahumada <gilbertsahumada@gmail.com>
2020-10-08 11:19:07 -0300

    seeder modified

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,10 @@
 <?php
+
+use App\Image;
 use App\Product;
+use App\Order;
+use App\User;
+use App\Payment;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +18,33 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         $count = 50;
-        factory(Product::class, $count)->create();
+        $products = factory(Product::class, $count)->create();
+
+        $users = factory(User::class, 20)
+                    ->create();
+                    
+
+        $orders = factory(Order::class, 10)
+                    ->make() 
+                    ->each(function($order) use ($users){        
+                        $order->customer_id = $users->random()->id;
+                        $order->save();
+
+                        $payment = factory(Payment::class)->make();
+                        $order->payment()->save($payment);
+                    });
+
+
+        /*$users = factory(User::class, $count)
+                    ->create()
+                    ->each(function($user ){
+                        $image = factory(Image::class)
+                            ->user()
+                            ->make();
+
+                        $user->image()->save($image);
+                    });
+        */
+          
     }
 }
