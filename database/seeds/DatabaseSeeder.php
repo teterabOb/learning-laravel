c8ea2bd14912274cf04dc026287b0e5d0027ac4b
Gilberts ahumada <gilbertsahumada@gmail.com>
2020-10-09 09:34:39 -0300

    seeder and image factory fixed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,3 +1,4 @@
+
 <?php
 
 use App\Cart;
@@ -16,49 +17,53 @@ class DatabaseSeeder extends Seeder
      * @return void
      */
     public function run()
-    {    
-        $users = factory(User::class, 20)
-                    ->create()
-                    ->each(function($user){
-                        $image = factory(Image::class)
-                            ->user()
-                            ->make();
-
-                        $user->image()->save($image);
-                    });;
-                    
-             
-        $orders = factory(Order::class, 10)
-                    ->make() 
-                    ->each(function($order) use ($users){        
-                        $order->customer_id = $users->random()->id;
-                        $order->save();
-
-                        $payment = factory(Payment::class)->make();
-                        $order->payment()->save($payment);
-                    });
-
-        $carts = factory(Cart::class, 20)->create();  
-                    
-        $products = factory(Product::class, 50)
-                    ->create()
-                    ->each(function($product) use ($orders, $carts){
-                        $order = $orders->random();
-
-                        $order->products()->attach([
-                            $product->id => ['quantity' => mt_rand(1, 3)]
-                        ]);
-
-                        $cart = $carts->random();
-
-                        $cart->products()->attach([
-                            $product->id => ['quantity' => mt_rand(1, 3)]
-                        ]);
-
-                        $images = factory(Image::class, 2)->make();
-                        $product->images()->saveMany($images);
-                    });
+    {
         
-          
+        $users = factory(User::class, 20)
+            ->create()
+            ->each(function ($user) {
+                $image = factory(Image::class)
+                    ->states('user')
+                    ->make();
+
+                $user->image()->save($image);
+            });
+
+        $orders = factory(Order::class, 10)
+            ->make()
+            ->each(function ($order) use ($users) {
+                $order->customer_id = $users->random()->id;
+                $order->save();
+
+                $payment = factory(Payment::class)->make();
+
+                // $payment->order_id = $order->id;
+                // $payment->save();
+
+                $order->payment()->save($payment);
+            });
+
+
+        $carts = factory(Cart::class, 20)->create();
+
+        $products = factory(product::class, 50)
+            ->create()
+            ->each(function ($product) use ($orders, $carts) {
+                $order = $orders->random();
+
+                $order->products()->attach([
+                    $product->id => ['quantity' => mt_rand(1, 3)]
+                ]);
+
+                $cart = $carts->random();
+
+                $cart->products()->attach([
+                    $product->id => ['quantity' => mt_rand(1, 3)]
+                ]);
+
+                $images = factory(Image::class, 20)->make();
+                //factory(User::class, 20)
+                $product->images()->saveMany($images);
+            });
     }
 }
